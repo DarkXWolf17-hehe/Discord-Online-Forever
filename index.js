fecf52d21cfd83687f2a7cfce8d0ce6822ec12e3
DarkXWolf17-hehe <maxhalston26@gmail.com>
2024-07-05 12:15:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris(process.env.MTA5ODY5NTg1NDQzMDM2NzgxNQ.Gev5MG.V8xSrLI7tGMNmODnIr57Z7wuow4hhGizZVzi2Y);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
